f9b365c023114cd269ab0dd2fe1317ef9f93783b
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-25 18:16:21 -0400

    refactored thunk logic into separate file for improved readability

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -8,6 +8,12 @@ import {
   removeUserFromLocalStorage,
 } from '../../utils/localStorage';
 
+import {
+  loginUserThunk,
+  registerUserThunk,
+  updateUserThunk,
+} from './userThunk';
+
 const initialState = {
   user: getUserFromLocalStorage(),
   isLoading: false,
@@ -17,47 +23,21 @@ const initialState = {
 export const registerUser = createAsyncThunk(
   'user/registerUser',
   async (user, thunkAPI) => {
-    try {
-      const response = await customFetch.post('/auth/register', user);
-
-      return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue({ error: error.response.data.msg });
-    }
+    return registerUserThunk('/auth/register', user, thunkAPI);
   }
 );
 
 export const loginUser = createAsyncThunk(
   'user/loginUser',
   async (user, thunkAPI) => {
-    try {
-      const response = await customFetch.post('/auth/login', user);
-      toast.success(`Welcome back ${response.data.user.name}!`);
-      return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue({ error: error.response.data.msg });
-    }
+    return loginUserThunk('/auth/login', user, thunkAPI);
   }
 );
 
 export const updateUser = createAsyncThunk(
   'user/updateUser',
   async (user, thunkAPI) => {
-    try {
-      const response = await customFetch.patch('/auth/updateUser', user, {
-        headers: {
-          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-        },
-      });
-      return response.data;
-    } catch (error) {
-      // console.log(error.response);
-      if (error.response.status === 401) {
-        thunkAPI.dispatch(logoutUser());
-        return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
-      }
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
+    return updateUserThunk('/auth/update', user, thunkAPI);
   }
 );
 
@@ -71,6 +51,7 @@ const userSlice = createSlice({
     logoutUser: (state) => {
       state.user = null;
       state.isSidebarOpen = false;
+      toast.success('Logout Successful!');
       removeUserFromLocalStorage();
     },
   },
