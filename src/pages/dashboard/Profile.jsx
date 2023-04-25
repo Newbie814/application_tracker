7c2102a05a8e095c8465e2883c63c13bd48e3ed9
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-25 16:45:24 -0400

    setup profile form in dashboard, as well as shared styles for all dashboard pages

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,4 +1,69 @@
+import Wrapper from './dashboardStyles/DashboardFormPage';
+import { useState } from 'react';
+import { FormRow } from '../../components';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { isLoading, user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState({
+    name: user?.name || '',
+    email: user?.email || '',
+    lastName: user?.lastName || '',
+    location: user?.location || '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, lastName, location } = userData;
+    if (!name || !email || !lastName || !location) {
+      toast.error('Please fill out all fields');
+      return;
+    }
+    console.log('submit');
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  return (
+    <Wrapper>
+      <form className='form' onSubmit={handleSubmit}>
+        <h3>Profile</h3>
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='name'
+            value={userData.name}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type='text'
+            labelText={'Last Name'}
+            name='lastName'
+            value={userData.lastName}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type='email'
+            name='email'
+            value={userData.email}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type='text'
+            name='location'
+            value={userData.location}
+            handleChange={handleChange}
+          />
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 export default Profile;
